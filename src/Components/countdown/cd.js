217e5e635632b6ea5d93692f524b7e6e661138c9
RuneKodehode <rune.kodehode@gmail.com>
2023-01-11 12:09:38 +0100

    wow

--- src/Components/countdown/cd.js
+++ src/Components/countdown/cd.js
@@ -1,22 +1,39 @@
+import { useState, useEffect } from "react";
+
 export function Timer() {
-  let countDownDate = new Date("Dec 24, 2023 00:00:00");
-  setInterval(function () {
-    let now = new Date().getTime();
-    let distance = countDownDate - now;
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    let Counter =
-      days + "dagar" + hours + "timar" + minutes + "min" + seconds + "s";
-    console.log(Counter);
-    return (
-      <div>
-        <div>{Counter}</div>
-        <div>123</div>
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      let countDownDate = new Date("Dec 24, 2023 00:00:00");
+      let now = new Date().getTime();
+      let distance = countDownDate - now;
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+  return (
+    <div>
+      <div id="countdown">
+        {`${time.days}`} dagar {`${time.hours}`} timar {`${time.minutes}`}{" "}
+        minuttar og {`${time.seconds}`} sekundar
       </div>
-    );
-  }, 1000);
+    </div>
+  );
 }
